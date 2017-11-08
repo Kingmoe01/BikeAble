4dc3415ddc76fc5f06485e731f93ea0fc115d98b
Mopise <moisetumu@gmail.com>
2017-11-08 15:16:03 -0500

    ReAdd project

--- BikeAble/obj/Release/Package/PackageTmp/scripts/newAdresse.js
+++
@@ -1,7 +0,0 @@
-﻿$(document).ready(function () {
-    $("#ctl00$ContentPlaceHolder1_no").tooltip();
-    $("#ctl00$ContentPlaceHolder1_street").tooltip();
-    $("#ctl00$ContentPlaceHolder1_postalCode").tooltip();
-    $("#ctl00$ContentPlaceHolder1_town").tooltip();
-    $("#ctl00$ContentPlaceHolder1_province").tooltip();
-});
